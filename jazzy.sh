c3df8167ce33a4e330571629fcfd8517f95baecb
Zhiyu Zhu/朱智语 <public-apollonian@outlook.com>
2020-10-20 22:48:12 -0400

    Replace Travis CI with GitHub Action

--- jazzy.sh
+++ jazzy.sh
@@ -9,7 +9,7 @@ jazzy \
       --github_url https://github.com/ApolloZhu/swift_qrcodejs \
       --github-file-prefix https://github.com/ApolloZhu/swift_qrcodejs/tree/${version} \
       --module-version ${version} \
-      --xcodebuild-arguments -target,swift_qrcodejs \
+      --swift-build-tool spm \
       --module swift_qrcodejs \
       --root-url https://apollozhu.github.io/swift_qrcodejs/ \
       --dash_url https://apollozhu.github.io/swift_qrcodejs/docsets/Srt2BilibiliKit.xml
